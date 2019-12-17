63f11ff5df41f6337f78136258bf6c5ea50c8538
vladsmetansky <vladsmetansky@gmail.com>
2019-12-17 18:29:32 +0200

    added ci and coverage

--- department-app/app.py
+++ department-app/app.py
@@ -3,5 +3,3 @@ from flask_sqlalchemy import SQLAlchemy
 
 if __name__ == '__main__':
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-    db = SQLAlchemy(app)
